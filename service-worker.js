2820d6330728b1651b568dbda501079b2c0823e1
almeldawy <almeldawy@gmail.com>
2025-06-09 10:29:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,10 @@
 const CACHE_NAME = 'radio-coran-v1';
 const urlsToCache = [
-  '/',
-  '/radio_du_Coran_Arabie_Saoudite.html',
-  '/favicon.png',
+  '/holy-quran-radio-pwa/', // Ajout de la racine du dépôt
+  '/holy-quran-radio-pwa/radio_du_Coran_Arabie_Saoudite.html', // Chemin complet
+  '/holy-quran-radio-pwa/favicon.png', // Chemin complet
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
-  // Vous pourriez vouloir mettre en cache le flux audio s'il s'agit d'un fichier statique, mais les flux en direct sont complexes
-  // 'https://stream.radiojar.com/4wqre23fytzuv' // La mise en cache des flux en direct est complexe et souvent peu pratique
 ];
 
 self.addEventListener('install', (event) => {
